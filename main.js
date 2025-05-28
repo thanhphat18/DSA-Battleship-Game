5467da1f2a3872aae9efc64929b263b85bfc1f01
Hue Lan <lanvunhuhue0912@gmail.com>
2025-05-28 13:48:48 +0700

    Chỉnh sửa lại các filefile

--- main.js
+++
@@ -1,6 +0,0 @@
-import { GameController } from './GameController.js';
-
-const playerBoardEl = document.getElementById("player-board");
-const enemyBoardEl = document.getElementById("enemy-board");
-
-const game = new GameController(playerBoardEl, enemyBoardEl);
